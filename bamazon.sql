99997785febd6e20882c6a7b13641206613e2738
Yadira Soto <lunar13heart@hotmail.com>
2019-07-01 21:05:42 -0400

    edited bamazon.sql by adding (100) & removing some NOTs

--- bamazon.sql
+++ bamazon.sql
@@ -4,11 +4,11 @@ CREATE database bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products (
-  item_id VARCHAR NOT NULL,
-  prouct_name VARCHAR NOT NULL,
-  department_name VARCHAR NOT NULL,
-  price INT NOT NULL,
-  stock_quantity INT NOT NULL,
+  item_id VARCHAR(100) NOT NULL,
+  proudct_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price INT NULL,
+  stock_quantity INT NULL,
   PRIMARY KEY (item_id)
 );
 
