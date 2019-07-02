799df741489e61990712944ba2411b3376f38046
Yadira Soto <lunar13heart@hotmail.com>
2019-07-02 17:51:59 -0400

    edited js file with comments/notes & js code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,20 +1,39 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
+
 
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
   user: "root",
 
-  // Your password
   password: "root",
   database: "bamazon_DB"
 });
 
-connection.connect(function(err) {
-    if (err) throw err;
-    runSearch();
-  });
+connection.connect(function (error) {
+  if (error) throw error;
+  // call function here from below
+  showItems();
+});
+
+function showItems() {
+  // going into database to select all. *=all
+  connection.query("SELECT * FROM products", function (error, response) {
+    if (error) throw error;
+    // show on page
+    console.table(response);
+
+  })
+  question();
+}
+
+function question(){
+
+}
+
+  // NPM INIT, NPM i mysql, npm i inquirer, npm console.table(look at webpage) TABLE.PUSH
+
