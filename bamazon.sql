2a5eecd6b87a19ed3cbac683a17232ca37a96a38
Yadira Soto <lunar13heart@hotmail.com>
2019-07-02 18:04:11 -0400

    changed the table information so it would work on Dbeaver

--- bamazon.sql
+++ bamazon.sql
@@ -14,33 +14,33 @@ CREATE TABLE products (
 
 
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Sunflowers", "Garden", 15, 150);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (1, "Sunflowers", "Garden", 15, 150);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Wedding Dress", "Formal Wear", 100, 1);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (2, "Wedding Dress", "Formal Wear", 100, 1);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Wedding Suit", "Formal Wear", 100, 1);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (3, "Wedding Suit", "Formal Wear", 100, 1);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Wedding Bands", "Jewelry", 150, 2);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (4, "Wedding Bands", "Jewelry", 150, 2);
 
-INSERT INTO products ( product_name, department_name, price, stock_quantity)
-VALUES ("Wedding Gifts", "Specialty Items", 20, 250);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (5, "Wedding Gifts", "Specialty Items", 20, 250);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Wedding Tables", "Dining", 30, 558);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (6, "Wedding Tables", "Dining", 30, 558);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Wedding Chairs", "Sitting", 20, 868);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (7, "Wedding Chairs", "Sitting", 20, 868);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Wedding Favors", "Specialty Items", 25, 674);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (8, "Wedding Favors", "Specialty Items", 25, 674);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Wedding Sparklers", "Fireworks", 8, 1542);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (9, "Wedding Sparklers", "Fireworks", 8, 1542);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Macarons", "Dessert", 7, 1237);
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES (10, "Macarons", "Dessert", 7, 1237);
 
